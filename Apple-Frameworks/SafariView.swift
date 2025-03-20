c980a46c952579772d3fcbb6180235547be9ff0b
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-03-20 13:24:35 -0500

    Implemented Safari View

---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by Юлия Дегтярева on 2025-03-19.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
