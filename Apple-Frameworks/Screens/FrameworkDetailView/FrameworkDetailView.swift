bb4a42d45c4ed31aee1ceb781b283379ba41d867
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-03-24 10:49:51 -0500

    Navigation Stack

--- Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -10,16 +10,10 @@ import SwiftUI
 struct FrameworkDetailView: View {
     
     var framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
-            
-            XDismissButton(isShowingDetailView: $isShowingDetailView)
-            
-            Spacer()
-            
             FrameworkTitleView(framework: framework)
             
             Text(framework.description)
@@ -31,8 +25,14 @@ struct FrameworkDetailView: View {
             Button {
                 isShowingSafariView = true
             } label: {
-                AFButton(title: "Learn More")
+//                AFButton(title: "Learn More")
+                Label("Learn More", systemImage: "book.fill")
             }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+//            .foregroundColor(.yellow)
+//            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .tint(.red)
         }
         .sheet(isPresented: $isShowingSafariView,
                content: {SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
@@ -42,7 +42,7 @@ struct FrameworkDetailView: View {
 
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+        FrameworkDetailView(framework: MockData.sampleFramework)
             .preferredColorScheme(.dark)
     }
 }
